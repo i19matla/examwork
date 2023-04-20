1534cf54c2d247c921af7d751ce45031bad8d4e8
i19matla <81630339+i19matla@users.noreply.github.com>
2023-04-20 17:08:56 +0200

    First json object two created
    First json object two created from json builder

---
+++ dataset2.js
@@ -0,0 +1,262 @@
+var datasetTwo = [
+    {
+        itemHeading: "Trädtoppar",
+        image: {
+            imgSrcUrl: "pictures/nature/trees.jpg",
+            imgAltText: "Trädtoppar sträcker sig mot himlen",
+            itemImageText: "Trädkrona mot himlen",
+            itemImageTitle: "Trädkrona mot himlen"
+        },
+        itemPrice: 299,
+        currency: "Kr",
+        itemDescription: "Trädtoppar mot himlen sträcker sig uppåt i full prakt, köp ett klätterträd.",
+        articleNr: "${Date.now()}"
+    },
+    {
+        itemHeading: "Alptoppar",
+        image: {
+            imgSrcUrl: "pictures/nature/alpspitze.jpg",
+            imgAltText: "Alptoppar",
+            itemImageText: "Alptoppar i solsken",
+            itemImageTitle: "Alptoppar i solsken"
+        },
+        itemPrice: 599,
+        currency: "Kr",
+        itemDescription: "Alptoppar och trädkronor i perfekt semesterkombination till överkomliga priser.",
+        articleNr: "2-1"
+    },
+    {
+        itemHeading: "Damm",
+        image: {
+            imgSrcUrl: "pictures/nature/pond.jpg",
+            imgAltText: "Damm",
+            itemImageText: "Odammig damm",
+            itemImageTitle: "Odammig damm"
+        },
+        itemPrice: 1999,
+        currency: "Kr",
+        itemDescription: "Damm helt utan damm, garanterat fri från kvalster. Kvalsterfritt i damm, garanterat. Perfekt till sommaren.",
+        articleNr: "2-2"
+    },
+    {
+        itemHeading: "Björkhängen",
+        image: {
+            imgSrcUrl: "pictures/nature/birchpendants.jpg",
+            imgAltText: "Björkhängen",
+            itemImageText: "Gröna björkhängen",
+            itemImageTitle: "Gröna björkhängen"
+        },
+        itemPrice: 19,
+        currency: "Kr",
+        itemDescription: "Björkhängen för att garantera björkens fortsatta tillväxt. Allergiovänligt men björkvänligt.",
+        articleNr: "2-3"
+    },
+    {
+        itemHeading: "Insektsmiddag",
+        image: {
+            imgSrcUrl: "pictures/nature/birdeatinsect.jpg",
+            imgAltText: "Fågel äter insekt",
+            itemImageText: "Insekter till middag",
+            itemImageTitle: "Insekter till middag"
+        },
+        itemPrice: 119,
+        currency: "Kr",
+        itemDescription: "Småfåglarnas favoriträtt, insekter. Brakmiddag med fin utsikt.",
+        articleNr: "2-4"
+    },
+    {
+        itemHeading: "Fågelmat",
+        image: {
+            imgSrcUrl: "pictures/nature/birdfood.jpg",
+            imgAltText: "Larv hänger ut i luften",
+            itemImageText: "Solande larv",
+            itemImageTitle: "Solande larv"
+        },
+        itemPrice: 20,
+        currency: "Kr",
+        itemDescription: "Larver perfekta att mata fåglarna i din trädgård. Mättar lika bra som en mask.",
+        articleNr: "2-5"
+    },
+    {
+        itemHeading: "Snöskvätta",
+        image: {
+            imgSrcUrl: "pictures/nature/birdoneleg.jpg",
+            imgAltText: "Fågel stående på ett ben",
+            itemImageText: "Snabb i luften, snabb på land",
+            itemImageTitle: "Snabb i luften, snabb på land"
+        },
+        itemPrice: 33,
+        currency: "Kr",
+        itemDescription: "Supersnabb fågel, springer lika fort på ett ben som den kan flyga. Snabbare än de flesta snabbflygande fåglar.",
+        articleNr: "2-6"
+    },
+    {
+        itemHeading: "Raststolpe",
+        image: {
+            imgSrcUrl: "pictures/nature/birdonpole.jpg",
+            imgAltText: "Fågel rastar på en stolpe",
+            itemImageText: "Fågelstolpe för en bra paus",
+            itemImageTitle: "Fågelstolpe för en bra paus"
+        },
+        itemPrice: 29,
+        currency: "Kr",
+        itemDescription: "Bekväm och bra fågelstolpe för de bästa pauserna. Lugn och go.",
+        articleNr: "2-7"
+    },
+    {
+        itemHeading: "Utsiktspinne",
+        image: {
+            imgSrcUrl: "pictures/nature/birdonstick.jpg",
+            imgAltText: "Fågel tittar ut från en kvist",
+            itemImageText: "Kvist anpassad för utsikt",
+            itemImageTitle: "Kvist anpassad för utsikt"
+        },
+        itemPrice: 99,
+        currency: "Kr",
+        itemDescription: "Till alla fåglar som älskar att speja efter lunchen med stabil grund på bra höjd.",
+        articleNr: "2-8"
+    },
+    {
+        itemHeading: "Hotell fågelinn",
+        image: {
+            imgSrcUrl: "pictures/nature/birdsatnest.jpg",
+            imgAltText: "Fåglar vid sitt bo",
+            itemImageText: "Bo med bra utsikt",
+            itemImageTitle: "Bo med bra utsikt"
+        },
+        itemPrice: 199,
+        currency: "Kr",
+        itemDescription: "Passar lika bra till kakaduor som gröngölingssångare. Lagom högt över mark, lagom bra.",
+        articleNr: "2-9"
+    },
+    {
+        itemHeading: "Skönsångare",
+        image: {
+            imgSrcUrl: "pictures/nature/birdsinger.jpg",
+            imgAltText: "Fågel sjunger för fullt",
+            itemImageText: "Fågelopera, första parkett",
+            itemImageTitle: "Fågelopera, första parkett"
+        },
+        itemPrice: 12,
+        currency: "Kr",
+        itemDescription: "Lyssna till en vacker opera ariang av en underbar skönsångar med toner som hämtade från himlen.",
+        articleNr: "2-10"
+    },
+    {
+        itemHeading: "Klätterträd",
+        image: {
+            imgSrcUrl: "pictures/nature/birdtreeclimb.jpg",
+            imgAltText: "Fågel klättrar i träd",
+            itemImageText: "Klätterträd gott nog åt fåglar",
+            itemImageTitle: "Klätterträd gott nog åt fåglar"
+        },
+        itemPrice: 250,
+        currency: "Kr",
+        itemDescription: "Ge dina fåglar en fantastisk plats att klättra runt på. Lika roligt som en flygtur till molnen.",
+        articleNr: "2-11"
+    },
+    {
+        itemHeading: "Hundboll",
+        image: {
+            imgSrcUrl: "pictures/nature/dogballrunning.jpg",
+            imgAltText: "Hund med boll i munnen",
+            itemImageText: "Boll lek",
+            itemImageTitle: "Boll lek"
+        },
+        itemPrice: 189,
+        currency: "Kr",
+        itemDescription: "Röd boll som är lika go att tugga på för hunden som det bästa hundbenet.",
+        articleNr: "2-12"
+    },
+    {
+        itemHeading: "Surt o gött",
+        image: {
+            imgSrcUrl: "pictures/nature/lime.jpg",
+            imgAltText: "Skuren lime staplat på hög",
+            itemImageText: "Gott med en skvätt socker",
+            itemImageTitle: "Gott med en skvätt socker"
+        },
+        itemPrice: 20,
+        currency: "Kr",
+        itemDescription: "Smakar bäst med en nypa socker till. Går att ha i drycken såväl som till maten.",
+        articleNr: "2-13"
+    },
+    {
+        itemHeading: "Rödfågel",
+        image: {
+            imgSrcUrl: "pictures/nature/redbird.jpg",
+            imgAltText: "Röd fågel sitter på kvist",
+            itemImageText: "Rödfågel, en livsnjutare",
+            itemImageTitle: "Rödfågel, en livsnjutare"
+        },
+        itemPrice: 99,
+        currency: "Kr",
+        itemDescription: "Rödfågel, skogens livsnjutare. Njuter mest hela dagen både på kvistar, pinnar och under sina flygturer.",
+        articleNr: "2-14"
+    },
+    {
+        itemHeading: "Sprattelmås",
+        image: {
+            imgSrcUrl: "pictures/nature/seabirdgroundflight.jpg",
+            imgAltText: "Fiskmås i flykten nära marken",
+            itemImageText: "Sprattligaste sprattelmåsen",
+            itemImageTitle: "Sprattligaste sprattelmåsen"
+        },
+        itemPrice: 289,
+        currency: "Kr",
+        itemDescription: "Sprattelmås, en udda sort av fiskmås. Utmärkta flygare och luftakrobater med sprattel som specialitet.",
+        articleNr: "2-15"
+    },
+    {
+        itemHeading: "Flygare",
+        image: {
+            imgSrcUrl: "pictures/nature/seagulflight.jpg",
+            imgAltText: "Mås ute och flyger",
+            itemImageText: "Flygtur ovan väg",
+            itemImageTitle: "Flygtur ovan väg"
+        },
+        itemPrice: 69,
+        currency: "Kr",
+        itemDescription: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus fugiat rem quas sit, impedit ad!",
+        articleNr: "2-16"
+    },
+    {
+        itemHeading: "Fågelrestaurang",
+        image: {
+            imgSrcUrl: "pictures/nature/seagulsonfield.jpg",
+            imgAltText: "Flock av måsar samlade på ett gärde",
+            itemImageText: "Måsbesök på fågelrestaurangen",
+            itemImageTitle: "Måsbesök på fågelrestaurangen"
+        },
+        itemPrice: 17,
+        currency: "Kr",
+        itemDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aspernatur asperiores quis facere voluptatibus quasi necessitatibus laboriosam!",
+        articleNr: "2-17"
+    },
+    {
+        itemHeading: "Naturupplevelse",
+        image: {
+            imgSrcUrl: "pictures/nature/waterfall.jpg",
+            imgAltText: "Vattenfall",
+            itemImageText: "Väl värt ett besök",
+            itemImageTitle: "Väl värt ett besök"
+        },
+        itemPrice: 78,
+        currency: "Kr",
+        itemDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem facere atque excepturi.",
+        articleNr: "2-18"
+    },
+    {
+        itemHeading: "Skönblomster",
+        image: {
+            imgSrcUrl: "pictures/nature/yellowwaterflowers.jpg",
+            imgAltText: "Gula blommor och gröna blad",
+            itemImageText: "Välgörande gulblomster",
+            itemImageTitle: "Välgörande gulblomster"
+        },
+        itemPrice: 199,
+        currency: "Kr",
+        itemDescription: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit soluta eos odit similique amet!",
+        articleNr: "2-19"
+    },
+];
