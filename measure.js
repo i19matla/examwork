e6d505cd89ab3ea1cc9a4dd2216e437af7b37de4
i19matla <81630339+i19matla@users.noreply.github.com>
2023-04-20 18:57:39 +0200

    Deeper measure test
    Better declaration of nodes

--- measure.js
+++ measure.js
@@ -1,10 +1,12 @@
 // ==UserScript==
-// @name     01 Mats measure script Exam
+// @name     02 Automatic jQuery Mats measure script jQuery Exam
 // @version  1
 // @grant    none
-// @include	 http://127.0.0.1:5500/vue.html
-// @include http://127.0.0.1:5500/jquery.html
+// @match http://127.0.0.1:5500/vue.html
+// @match http://127.0.0.1:5500/jquery.html
+// @description Render time script
 // ==/UserScript==
+
 
 const bodyEle = document.getElementsByTagName("body")[0];
 
@@ -13,20 +15,35 @@ const configuration = {
     childList: true,
     subtree: true
 };
-var timer1 = 0;
-var timer2 = 0;
+var tamperTimer1 = 0;
+var tamperTimer2 = 0;
+var tamperTimeTotal = 0;
+var tamperCounter = 0;
+var nodesChanged;
+let indexa = 0;
 
 const callback = (mutationList, listen) => {
-    timer1 = Date.now();
+    tamperTimer1 = Date.now();
     for (const mutation of mutationList) {
         if (mutation.type === "childList") {
-            console.log("Added/removed a child node");
+            //console.log("Added/removed a child node");
+            //console.log(mutation.addedNodes[0]);
+            //console.log(mutation.removedNodes[0]);
+            console.log("xxxxxxxxxxxxxxxRemovedxxxxxxxxxxxxxxxxxxxxx" + mutation.removedNodes.forEach(node => nodesChanged = node));
+            console.log(mutation.addedNodes.forEach(node => nodesChanged = node));
         } else if (mutation.type === "attributes") {
-            console.log(`${mutation.attributeName} was modified.`);
+            console.log(`The ${mutation.attributeName} attribute was modified.`);
         }
     }
-    timer2 = Date.now();
-    console.log(timer2-timer1);
+    tamperTimer2 = Date.now();
+    tamperTimeTotal += tamperTimer2-tamperTimer1;
+   // console.log("XXXXXXXXXXXXXXXXXX   Tiden " + tamperTimeTotal);
+   // console.log(tamperCounter);
+    tamperCounter++;
+    if (tamperCounter > 0) {
+        tamperTimeTotal = 0;
+        tamperCounter = 0;
+    }
 };
 
 const listen = new Mutationlisten(callback);
